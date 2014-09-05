1d4c017a69ba6636c5e3c4f2a4d25299e6e48a3a
Denis R <Denisov21@users.noreply.github.com>
2014-09-05 19:20:07 +0200

    Create strings.js

---
+++ strings.js
@@ -0,0 +1,13 @@
+/*
+* strings.js
+*
+* Made available under a MIT License:
+* http://www.opensource.org/licenses/mit-license.php
+*
+*/
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, regexp: true, indent: 4, maxerr: 50 */
+/*global define, $, brackets, window */
+define(function (require, exports, module) {
+"use strict";
+module.exports = require("i18n!nls/strings");
+});
